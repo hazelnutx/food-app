5c55da66e65fcebb403e506ebe8a4df168cf9672
Kasper Sandin <kasper.sandin@gmail.com>
2021-01-31 23:28:34 +0100

    Zero page margin/padding

--- src/components/GlobalStyle.ts
+++ src/components/GlobalStyle.ts
@@ -1,6 +1,10 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+  html, body {
+    padding: 0;
+    margin: 0;
+  }
   body {
     background-color: ${({ theme }) => theme.palette.background.default};
     color: ${({ theme }) => theme.palette.text.primary};
