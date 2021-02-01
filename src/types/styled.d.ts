c5fcb8e0c3420a8074d384f2f9c898153950c501
Kasper Sandin <kasper.sandin@gmail.com>
2021-02-01 21:33:00 +0100

    Explain styled-components type augmentation

--- src/types/styled.d.ts
+++ src/types/styled.d.ts
@@ -1,6 +1,8 @@
 import "styled-components";
 import { Theme } from "../Theme";
 
+// Type augmentation to make styled-components use our applications Theme type
+
 declare module "styled-components" {
   export interface DefaultTheme extends Theme {}
 }
